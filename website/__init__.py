b28bcc24a4a2ba048d8a05bc1d7e70b917a5e8b9
Nathan Abraham <abnathan123@gmail.com>
2021-06-30 16:25:41 -0400

    Fixed indentation

--- website/__init__.py
+++ website/__init__.py
@@ -4,19 +4,20 @@ import os
 # Google maps API key
 MAPS_API_KEY = ""
 
+
 def create_app():
-	global MAPS_API_KEY
+    global MAPS_API_KEY
 
-	# Initalize Flask Object
-	app = Flask(__name__)
-	with open("key.txt") as key:
-		app.config["SECRET_KEY"] = key.readline()
-		# Read key
-		MAPS_API_KEY = key.readline()
+    # Initalize Flask Object
+    app = Flask(__name__)
+    with open("key.txt") as key:
+        app.config["SECRET_KEY"] = key.readline()
+        # Read key
+        MAPS_API_KEY = key.readline()
 
-	from .views import views
-	
-	# Register views.py
-	app.register_blueprint(views, url_prefix="/")
+    from .views import views
 
-	return app
+    # Register views.py
+    app.register_blueprint(views, url_prefix="/")
+
+    return app
