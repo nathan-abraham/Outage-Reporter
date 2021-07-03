a92a10b1626971ea6ed9d5c18d7dd81fcee8c6f6
Nathan Abraham <abnathan123@gmail.com>
2021-07-02 20:05:58 -0400

    Updated __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 import os
 
 # Google maps API key
 MAPS_API_KEY = ""
+DB_NAME = "database.db"
+db = SQLAlchemy()
+reports = None
 
 
 def create_app():
@@ -12,12 +16,24 @@ def create_app():
     app = Flask(__name__)
     with open("key.txt") as key:
         app.config["SECRET_KEY"] = key.readline()
+        app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
         # Read key
         MAPS_API_KEY = key.readline()
+
+    db.init_app(app)
 
     from .views import views
 
     # Register views.py
     app.register_blueprint(views, url_prefix="/")
 
+    create_database(app)
+
     return app
+
+
+def create_database(app):
+    # If database doesn't already exist, then create it
+    if not os.path.exists("website/" + DB_NAME):
+        db.create_all(app=app)
+        print("Created database")
